354dd9066fe69e49d01bcbeb95a852d580f36b7e
vouvoyer <nicholas.dimitroff@gmail.com>
2015-03-08 12:01:57 -0700

    just some notes

---
+++ lint.tcl
@@ -0,0 +1,14 @@
+# look for bare $variables rather than ${var}
+
+
+# look for redefinition command
+
+# list all variables, sorted
+# mention their length
+
+# list all procedure names, sorted
+# bracket depth checker
+# count lines, whitespace, comments
+# watch for no indentations -> fix indentation based on bracket depth
+# count inline comments
+# find comments containing likely code: { }
